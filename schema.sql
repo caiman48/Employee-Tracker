8bc59ba977c754910881bb10fb33fc635370f2b2
caiman48 <caiman48clm@gmail.com>
2024-04-23 20:04:47 -0400

    added schema.sql

---
+++ schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS Team_db;
+CREATE DATABASE Team_db;
+USE Team_db;
+
+
+CREATE TABLE departments (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(48) NOT NULL
+);
+
+CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(48) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES departments(id)
+);
+
+CREATE TABLE employees (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(48) NOT NULL,
+  last_name VARCHAR(48) NOT NULL,
+  role_id INT,
+  manager_id INT NULL,
+  FOREIGN KEY (role_id) REFERENCES roles(id),
+  FOREIGN KEY (manager_id) REFERENCES employees(id)
+);
